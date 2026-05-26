0e21a4f876f5bd810833dfebb523ba13b249c815
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-05-26 08:55:58 +0530

    added webhook verification

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI,Request
+from fastapi.responses import PlainTextResponse
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = FastAPI()
+
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
 @app.get("/")
 def home():
     return {"message": "WhatsApp AI Automation Running"}
+
+
+
+@app.get("/webhook")
+async def verify_webhook(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return PlainTextResponse(challenge)
+
+    return {"error": "Verification failed"}
+
+
 
 @app.post("/webhook")
 async def webhook(request: Request):
